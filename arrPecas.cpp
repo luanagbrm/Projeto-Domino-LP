8d25033e4b4f7c32014895361f65112d57bfd41c
Luana Macedo <92169736+luanagbrm@users.noreply.github.com>
2024-08-08 11:17:14 -0300

    Upload - Criação de peças

---
+++ arrPecas.cpp
@@ -0,0 +1,31 @@
+typedef struct{
+	int ladoA;
+	int ladoB;
+	int status;
+}Peca;
+
+Peca array[28];
+
+void criarPecas(Peca peca, array[28]){
+	int j = 0;
+	
+	for(int i = 0; i < 6; i++){
+		for(int k = i; k < 6; k++){
+			Peca.ladoA[i];
+			Peca.ladoB[k];
+			array[j] = Peca;
+		}
+	}
+}
+
+int main(){
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
