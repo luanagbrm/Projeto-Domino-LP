b478743d24965d543426cd0de7f0d735c452e4dd
bialim <beatrizlima.blm@gmail.com>
2024-08-14 12:08:48 -0300

    Dom_BGLL_Projeto_Controller.h

---
+++ Dom_BGLL_Projeto_Controller.h
@@ -0,0 +1,17 @@
+/*  
+DOM-BGLL - Projeto Dominó - Etapa 2
+14/08/2024 - Grupo:BGLL
+
+Nome dos integrantes:
+
+- Beatriz Lima Morais
+- Grazielle Batista de Almeida
+- Luana Gabrielle Rodrigues Macedo
+- Lucas Ferri dos Santos
+
+A Etapa 2 consiste de criar 7 arquivos com a finalidade de separar o projeto de acordo com seu proposito.
+A parte destinada à funcionalidade do programa está destinada a pasta Controller.h, a qual contem os protótipos das funções.
+
+*/
+
+void embaralharPecas(Carta domino[28]);
