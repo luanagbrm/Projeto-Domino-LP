f7b8f770ee525ddf586a84f74453b513ebb5cd53
GaiaOcean <86306468+GaiaOcean@users.noreply.github.com>
2024-08-14 12:46:32 -0300

    Update Dom_BGLL_Projeto_Model.h

--- Dom_BGLL_Projeto_Model.h
+++ Dom_BGLL_Projeto_Model.h
@@ -1,1 +1,4 @@
+//Prototipo das funcoes utilizadas no model.cpp
 void criarCarta(Carta domino[28]);
+void embaralharPecas(Carta domino[28]);
+void pieceGiveAway(Carta totalPieces[28], Player players[2], int numPlayers);
