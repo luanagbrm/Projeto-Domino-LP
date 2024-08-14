21d38035ba402e0a2dcfb32b8872425b1a6584b8
GaiaOcean <86306468+GaiaOcean@users.noreply.github.com>
2024-08-14 12:53:56 -0300

    Update Dom_BGLL_Projeto_Model.cpp

--- Dom_BGLL_Projeto_Model.cpp
+++ Dom_BGLL_Projeto_Model.cpp
@@ -1,25 +1,43 @@
-//gerar pecas
 
 #include "Dom_BGLL_Projeto_Model.h"
+#include <stdlib.h>
+#include <time.h>
 
-//structure sobre as informações de cada carta do dominó
-typedef struct{
-	int ladoA;
-	int ladoB;
-	int status;
-}Carta;
+void criarCarta(Carta domino[28]) {
+    Carta peca;
+    int j = 0;
 
-//função para criar cartas
-void criarCarta(Carta domino[28]){
-	Carta peca;
-	int j = 0;
-	
-        for(int i = 0; i <= 6; i++){
-            for(int k = i; k <= 6; k++){
-		peca.ladoA = i;
-		peca.ladoB = k;
-		domino[j] = peca;
-		j++;
-		}
-	}
+    for (int i = 0; i <= 6; i++) {
+        for (int k = i; k <= 6; k++) {
+            peca.ladoA = i;
+            peca.ladoB = k;
+            domino[j] = peca;
+            j++;
+        }
+    }
+}
+
+void embaralharPecas(Carta domino[28]) {
+    Carta peca2;
+    int p;
+
+    for (int i = 0; i < 28; i++) {
+        peca2 = domino[i];
+        p = rand() % 28;
+        domino[i] = domino[p];
+        domino[p] = peca2;
+    }
+}
+
+void pieceGiveAway(Carta totalPieces[28], Player players[2], int numPlayers) {
+    int pieceAssign = 0;
+
+    for (int i = 0; i < numPlayers; i++) {
+        players[i].numPieces = 0;
+
+        for (int k = 0; k < 7 && pieceAssign < 28; k++) {
+            players[i].hand[k] = totalPieces[pieceAssign++];
+            players[i].numPieces++;
+        }
+    }
 }
