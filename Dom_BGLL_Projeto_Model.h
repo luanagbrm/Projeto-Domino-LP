900a17adb33bda6aa5a4389dfe184716f612f7bf
GaiaOcean <86306468+GaiaOcean@users.noreply.github.com>
2024-08-14 12:51:37 -0300

    Update Dom_BGLL_Projeto_Model.h

--- Dom_BGLL_Projeto_Model.h
+++ Dom_BGLL_Projeto_Model.h
@@ -1,3 +1,15 @@
+//Estruturas importatantes que irao serem constantemente usadas tanto no controller quanto no view
+typedef struct {
+    int ladoA;
+    int ladoB;
+    int status; 
+} Carta;
+
+typedef struct {
+    Carta hand[7];  
+    int numPieces;  
+} Player;
+
 //Prototipo das funcoes utilizadas no model.cpp
 void criarCarta(Carta domino[28]);
 void embaralharPecas(Carta domino[28]);
