5c002852c7d388dfbd2f5742f50e87e18d4ca756
bialim <beatrizlima.blm@gmail.com>
2024-08-20 16:48:12 -0300

    Dom_BGLL_Projeto_View.h

--- ProjetoDOM/Dom_BGLL_Projeto_View.h
+++ ProjetoDOM/Dom_BGLL_Projeto_View.h
@@ -1,2 +1,16 @@
+/*
+DOM-BGLL - Projeto Dominó - Etapa 2
+20/08/2024 - Grupo:BGLL
+
+Nome dos integrantes:
+
+- Beatriz Lima Morais
+- Grazielle Batista de Almeida
+- Luana Gabrielle Rodrigues Macedo
+- Lucas Ferri dos Santos
+
+ O View.h contém os protótipos das funções responsaveis pela visualização dos dados e interação com o usuário.
+*/
+
 void mostrarCartas(Carta domino[28]);
 void mostrarMenu();
