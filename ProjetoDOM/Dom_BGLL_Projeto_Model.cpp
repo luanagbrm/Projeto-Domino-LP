9294ee49ddc723dbee3a868f1544eb3fee3331c3
bialim <beatrizlima.blm@gmail.com>
2024-08-20 16:44:33 -0300

    Dom_BGLL_Projeto_Model.cpp

--- ProjetoDOM/Dom_BGLL_Projeto_Model.cpp
+++ ProjetoDOM/Dom_BGLL_Projeto_Model.cpp
@@ -1,7 +1,21 @@
-#include "Dom_BGLL_Projeto_Model.h"
+/*
+DOM-BGLL - Projeto Dominó - Etapa 2
+20/08/2024 - Grupo:BGLL
 
+Nome dos integrantes:
+
+- Beatriz Lima Morais
+- Grazielle Batista de Almeida
+- Luana Gabrielle Rodrigues Macedo
+- Lucas Ferri dos Santos
+
+ A pasta Model contém os dados e estruturas que serão manipulados
+*/
+
+#include "Dom_BGLL_Projeto_Model.h"
 #include <stdlib.h>
 
+//função para criar as peças(cartas) do dominó
 void criarCarta(Carta domino[28]) {
     Carta peca;
     int j = 0;
