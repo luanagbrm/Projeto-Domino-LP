fb3021b8ff8f1fa68887187f1112ccb930d91e47
Lucas Ferri <77448120+Ferri-js@users.noreply.github.com>
2024-08-27 22:22:06 -0300

    Cabeçalho

--- ProjetoDOM/Dom_BGLL_Projeto.cpp
+++ ProjetoDOM/Dom_BGLL_Projeto.cpp
@@ -1,3 +1,15 @@
+/*
+DOM-BGLL - Projeto Dominó - Etapa 3
+27/08/2024 - Grupo:BGLL
+
+Nome dos integrantes:
+
+- Beatriz Lima Morais
+- Grazielle Batista de Almeida
+- Luana Gabrielle Rodrigues Macedo
+- Lucas Ferri dos Santos
+*/
+
 #include "Dom_BGLL_Projeto_Controller.cpp"
 
 
--- ProjetoDOM/Dom_BGLL_Projeto_View.h
+++ ProjetoDOM/Dom_BGLL_Projeto_View.h
@@ -1,3 +1,15 @@
+/*
+DOM-BGLL - Projeto Dominó - Etapa 3
+27/08/2024 - Grupo:BGLL
+
+Nome dos integrantes:
+
+- Beatriz Lima Morais
+- Grazielle Batista de Almeida
+- Luana Gabrielle Rodrigues Macedo
+- Lucas Ferri dos Santos
+*/
+
 #ifndef DOM_BGLL_PROJETO_VIEW
 #define DOM_BGLL_PROJETO_VIEW
 
